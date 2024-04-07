729ed9dee5da14f6d1af09090221ad0fd2438c91
orange <970482444@qq.com>
2024-04-07 22:47:18 +0800

    fea2: 过滤

--- src/funcs/compress.ts
+++ src/funcs/compress.ts
@@ -1,8 +1,13 @@
 import * as sharp from "sharp";
 import { readDirPath } from "../utils/index.ts";
 import * as fs from "fs/promises";
-import { logInfo } from "../utils/log.ts";
+import { logInfo, logSuccessInfo } from "../utils/log.ts";
 import ora from 'ora';
+
+/**
+ * 支持的扩展名
+ */
+const ExpectExtendName = ['png', 'jpg', 'jpeg'];
 
 /**
  * 先实现jpg、png的压缩，简易版实现。
@@ -23,7 +28,16 @@ export const compress = async () => {
   }, 1000);
   // 调用sharp压缩图片
   // 1. 针对非jpg、png过滤
-
+  const filteredDir = dir.filter(fileName => {
+    const splitFileName = fileName.split('.');
+    const extName = splitFileName[splitFileName.length - 1];
+    if (ExpectExtendName.includes(extName)) {
+      return true;
+    }
+    return false;
+  })
+  logSuccessInfo('filered Dir: ');
+  logInfo(filteredDir)
 
   // 2. 循环将文件读取成<Buffer>，传递给sharp并输出。
   // 这里可以封装一个函数，将单个文件给sharp处理
